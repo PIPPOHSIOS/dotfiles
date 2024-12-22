29acc44b498b7d5da34cd2d05947ee152bda0cc8
PIPPOSHIOS <andreagatto2004@gmail.com>
2024-12-22 17:41:35 +0100

    Initial commit

---
+++ private_dot_config/nvim/lua/configs/conform.lua
@@ -0,0 +1,22 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { "prettier" },
+    html = { "prettier" },
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+  },
+
+  -- Set default options
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return options
---
+++ private_dot_config/nvim/lua/options.lua
@@ -0,0 +1,16 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+local opt = vim.o
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.shiftround = false
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
